ab2f78fa194bdc9b3931f5d6c8bbc172d3f816aa
Dogeek <simon.bordeyne@gmail.com>
2025-06-22 08:36:34 +0200

    terraform: databases import missing state

---
+++ terraform/workspaces/databases/imports.tf
@@ -0,0 +1,57 @@
+import {
+  to = module.database.postgresql_role.roles["prowlarr"]
+  id = "prowlarr"
+}
+
+import {
+  to = module.database.postgresql_database.databases["keycloak-keycloak"]
+  id = "keycloak"
+}
+import {
+  to = module.database.postgresql_database.databases["sonarr-sonarr_log"]
+  id = "sonarr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["sonarr-sonarr"]
+  id = "sonarr"
+}
+import {
+  to = module.database.postgresql_database.databases["radarr-radarr_log"]
+  id = "radarr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["radarr-radarr"]
+  id = "radarr"
+}
+import {
+  to = module.database.postgresql_database.databases["whisparr-whisparr_log"]
+  id = "whisparr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["whisparr-whisparr"]
+  id = "whisparr"
+}
+import {
+  to = module.database.postgresql_database.databases["readarr-readarr_log"]
+  id = "readarr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["readarr-readarr"]
+  id = "readarr"
+}
+import {
+  to = module.database.postgresql_database.databases["bazarr-bazarr_log"]
+  id = "bazarr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["bazarr-bazarr"]
+  id = "bazarr"
+}
+import {
+  to = module.database.postgresql_database.databases["prowlarr-prowlarr_log"]
+  id = "prowlarr_log"
+}
+import {
+  to = module.database.postgresql_database.databases["prowlarr-prowlarr"]
+  id = "prowlarr"
+}
